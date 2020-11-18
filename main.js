ca98cc9f8551d7d0e4c34b039b0d4d292c51b579
gabriele <gabrielescodinu96@gmail.com>
2020-11-18 15:14:49 +0100

    commit 02

--- main.js
+++ main.js
@@ -18,4 +18,7 @@ for (var i=0; i<lista.length; i++) {
   var listaContainer = document.getElementById('lista-container').innerHTML;
   document.getElementById('lista-container').innerHTML = listaContainer + "<li>" + item + "</li>";
 }
+
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+var position = lista.indexOf(cognome);
+document.getElementById('posizione').innerHTML = position;

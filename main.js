0cf56e9d350afe92faa3471c07e5897f3de286a0
gabriele <gabrielescodinu96@gmail.com>
2020-11-18 15:09:09 +0100

    commit 01

---
+++ main.js
@@ -0,0 +1,21 @@
+// Chiedere all’utente il cognome
+var cognome = prompt("Inserisci il tuo cognome");
+
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+var lista = [
+  "Bianchi",
+  "Rossi",
+  "Duzioni",
+  "Balsano",
+  "Verdi"
+];
+lista.push(cognome);
+console.log(lista);
+
+// stampa la lista ordinata alfabeticamente
+for (var i=0; i<lista.length; i++) {
+  var item = lista[i];
+  var listaContainer = document.getElementById('lista-container').innerHTML;
+  document.getElementById('lista-container').innerHTML = listaContainer + "<li>" + item + "</li>";
+}
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
